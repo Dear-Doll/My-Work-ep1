90fcc8005a7961c72cd411b524800cd18346fd59
Dear-Doll <1415969390@qq.com>
2022-09-30 22:21:05 +0800

    9/30 22:20 second

--- app/src/main/java/com/example/myclass/MainActivity.java
+++ app/src/main/java/com/example/myclass/MainActivity.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
@@ -19,6 +20,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private LinearLayout linearLayout1,linearLayout2,linearLayout3,linearLayout4;
     private Fragment fragment1,fragment2,fragment3,fragment4;
+    private ImageView imageView1,imageView2,imageView3,imageView4;
 
     private FragmentManager manager;
     private FragmentTransaction transaction;
@@ -33,7 +35,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 //            getWindow().setFlags(
 //                    WindowManager.LayoutParams.FLAG_FULLSCREEN,
 //                    WindowManager.LayoutParams.FLAG_FULLSCREEN
-//            );
+//            );          #隐藏状态栏
         }
 
         linearLayout1=findViewById(R.id.LinearLayout1);
@@ -46,11 +48,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         fragment3=new Fragment3();
         fragment4=new Fragment4();
 
+        imageView1=findViewById(R.id.imageView);
+        imageView2=findViewById(R.id.imageView2);
+        imageView3=findViewById(R.id.imageView3);
+        imageView4=findViewById(R.id.imageView4);
+
         manager=getSupportFragmentManager();
 
         initial();
 
-        hidden();
+        transaction=manager.beginTransaction()
+                .hide(fragment2)
+                .hide(fragment3)
+                .hide(fragment4);
+        transaction.commit();
 
         linearLayout1.setOnClickListener(this);
         linearLayout2.setOnClickListener(this);
@@ -88,12 +99,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         switch (i){
             case 1:showfragment(fragment1);
+                onchosen(imageView1);
                 break;
             case 2:showfragment(fragment2);
+                onchosen(imageView2);
                 break;
             case 3:showfragment(fragment3);
+                onchosen(imageView3);
                 break;
             case 4:showfragment(fragment4);
+                onchosen(imageView4);
                 break;
         }
     }
@@ -101,6 +116,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void showfragment(Fragment fragment) {
         transaction.show(fragment);
     }
+    private void onchosen(ImageView imageView) {imageView.setColorFilter(getColor(R.color.wechat_green));}
 
     private void hidden() {
         transaction=manager.beginTransaction()
@@ -109,7 +125,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 .hide(fragment3)
                 .hide(fragment4);
         transaction.commit();
-
+        imageView1.setColorFilter(getColor(R.color.default_grey));
+        imageView2.setColorFilter(getColor(R.color.default_grey));
+        imageView3.setColorFilter(getColor(R.color.default_grey));
+        imageView4.setColorFilter(getColor(R.color.default_grey));
     }
 
 
