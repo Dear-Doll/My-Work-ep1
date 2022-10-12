af7c0f3057ae90ea7a1aa3d758103d2a7851cb25
Dear-Doll <1415969390@qq.com>
2022-10-12 21:05:41 +0800

    recyclerView updates

---
+++ app/src/main/java/com/example/myclass/Adapter.java
@@ -0,0 +1,63 @@
+package com.example.myclass;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+
+    private View itemview;
+    private List<String> list;
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        itemview = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_list1, parent, false);
+
+        return new ViewHolder(itemview);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.textView1.setText("" + position);
+        holder.textView2.setText(list.get(position));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public void list(List<String> list) {
+        this.list = list;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView textView1, textView2;
+        private ImageView imageView_item1;
+        private LinearLayout linearLayout_item1;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textView1 = itemView.findViewById(R.id.textView_item1);
+            textView2 = itemView.findViewById(R.id.textView_item2);
+            imageView_item1 = itemView.findViewById(R.id.imageView_item1);
+            linearLayout_item1 = itemView.findViewById(R.id.linearLayout_item1);
+        }
+    }
+
+
+}
--- app/src/main/java/com/example/myclass/Fragment2.java
+++ app/src/main/java/com/example/myclass/Fragment2.java
@@ -1,17 +1,46 @@
 package com.example.myclass;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Fragment2 extends Fragment {
+
+    private RecyclerView recyclerView;
+    private Adapter myadapter;
+    private List<String> list = new ArrayList<>();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_2, container, false);
+        View view;
+        view = inflater.inflate(R.layout.fragment_2, container, false);
+        recyclerView = view.findViewById(R.id.recyclerView1);
+        listdata();
+        myadapter = new Adapter();
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this.getActivity());
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(myadapter);
+        myadapter.list(list);
+        return view;
+
+    }
+
+    private void listdata() {
+        for(int i=0;i<=24;i++){
+            list.add("the "+i+" of contacter");
+        }
     }
 }
